d9b2bdcebe5ea83194998db83a46ded82ff7db3a
Muhammed Sanjid <sanjid357@gmail.com>
2025-06-13 18:24:40 +0530

    footer ui updated

--- components/BorderDiv.tsx
+++ components/BorderDiv.tsx
@@ -1,12 +1,16 @@
-import React from 'react'
-import { cn } from '@/lib/utils'
+import React from "react";
+import { cn } from "@/lib/utils";
 
-const BorderDiv = ({children,className}: {children: React.ReactNode,className?: string}) => {
+const BorderDiv = ({
+  children,
+  className,
+}: {
+  children: React.ReactNode;
+  className?: string;
+}) => {
   return (
-    <div className={cn("p-2 rounded-3xl border-2",className)}>
-      {children}
-    </div>
-  )
-}
+    <div className={cn("rounded-3xl border-2 p-2 bg-primary/5 backdrop-blur-md", className)}>{children}</div>
+  );
+};
 
-export default BorderDiv
+export default BorderDiv;
